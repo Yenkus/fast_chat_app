88c4f7d2dad71fd001ba50aad956e6eac5aec6b4
yenkus <mryenkus@gmail.com>
2024-07-10 00:37:06 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fast_chat_app/pages/home_page.dart';
+import 'package:fast_chat_app/themes/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MultiProvider(providers: [
+    // Theme provider
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+    )
+  ], child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const HomePage(),
+    );
+  }
+}
