8b3f1a5d8300e13be8823a5ae0d34a9f9483482b
yenkus <mryenkus@gmail.com>
2024-07-14 16:04:54 -0700

    Can now send messages in the app, Thank God.

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,21 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fast_chat_app/components/my_text_field.dart';
 import 'package:fast_chat_app/services/auth/auth_service.dart';
 import 'package:fast_chat_app/services/chat_services/chat_service.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   final String receiverEmail;
   final String receiverID;
-  ChatPage({
+  const ChatPage({
     super.key,
     required this.receiverEmail,
     required this.receiverID,
   });
 
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   // text editing controller
   final TextEditingController _messageController = TextEditingController();
 
   // chat & auth services
   final ChatService _chatService = ChatService();
+
   final AuthService _authService = AuthService();
 
   // send message
@@ -23,7 +31,8 @@ class ChatPage extends StatelessWidget {
     // if there is something inside the textfield
     if (_messageController.text.isNotEmpty) {
       // send message
-      await _chatService.sendMessage(receiverID, _messageController.text);
+      await _chatService.sendMessage(
+          widget.receiverID, _messageController.text);
 
       // clear the controller
       _messageController.clear();
@@ -34,8 +43,66 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(receiverEmail),
+        title: Text(widget.receiverEmail),
+      ),
+      body: Column(
+        children: [
+          // display all messages
+          Expanded(child: _buildMessageList()),
+
+          // user input
+          _buildUserInput(),
+        ],
       ),
     );
+  }
+
+  // build message list
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+        stream: _chatService.getMessages(widget.receiverID, senderID),
+        builder: (context, snapshot) {
+          // errors
+          if (snapshot.hasError) {
+            return const Text("Error");
+          }
+
+          // loading..
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text("Loading");
+          }
+
+          // return message
+          return ListView(
+            children: snapshot.data!.docs
+                .map((doc) => _buildMessageItem(doc))
+                .toList(),
+          );
+        });
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  // build message input
+  Widget _buildUserInput() {
+    return Row(children: [
+      // textfield should take up most of the space
+      Expanded(
+        child: MyTextField(
+            textEditingController: _messageController,
+            hintText: "Type a message"),
+      ),
+
+      // send button
+      IconButton(
+        onPressed: () => sendMessage(),
+        icon: const Icon(Icons.send),
+      ),
+    ]);
   }
 }
